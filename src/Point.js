abbc0f5171379c9686e740f68b5e784ded3067e4
nltesown <nlte@magic.fr>
2014-02-13 18:18:34 +0100

    Use moment.js

--- src/Point.js
+++ src/Point.js
@@ -5,8 +5,18 @@
 var Point = (function () {
 	var self;
 
-	var constructor = function (options) {
+	var constructor = function (dateTime, options) {
 		self = this;
+
+		if (typeof dateTime === "undefined" || !dateTime._isAMomentObject) {
+			throw new Error("dateTime must be a moment.js object");
+		}
+
+		self.dateTime = moment().unix(dateTime);
+
+
+
+
 	};
 
 	constructor.prototype = {
