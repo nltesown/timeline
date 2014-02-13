540baa5b5fdd84b35999bead58106866f4632211
nltesown <nlte@magic.fr>
2014-02-13 12:42:44 +0100

    Update Timeline.js, create Point.js

---
+++ src/Point.js
@@ -0,0 +1,18 @@
+/*
+ * Point
+ */
+
+var Point = (function () {
+	var self;
+
+	var constructor = function (options) {
+		self = this;
+	};
+
+	constructor.prototype = {
+	};
+
+	// Add methods here
+
+	return constructor;
+}());
--- src/Timeline.js
+++ src/Timeline.js
@@ -19,26 +19,49 @@ var Timeline = (function () {
 		options = $.extend(defaults, options);
 
 		elem = $("<div class='timeline'></div>").appendTo(options.container);
-		elem.css({ backgroundColor: options.backgroundColor});
+		elem.css({ backgroundColor: options.backgroundColor });
+
+		points = [];
 
 		self.elem = elem;
+		self.points = points;
 	};
 
 	constructor.prototype = {
 		version: "0.1",
+		addPoint: addPoint,
 		hide: hide,
 		show: show
 	};
 
-	return constructor;
+	// addPoint
+	// @param points `Point` object
+	function addPoint (point) {
+
+		if (point instanceof Point === false) {
+			throw new Error("Adding a point requires a Point object.");
+		}
+
+
+
+		$("<div class='timeline-point'></div>").appendTo(self.elem);
+		self.points.push(point);
+
+
+	}
+
+
+
 
 	function hide () {
 		self.elem.hide();
 	}
 
+
 	function show () {
 		self.elem.show();
 	}
 
+	return constructor;
 }());
 
